8ef6ec22a3fbff91e630ee8853e3763df22e2440
hmbp <tkk@hongo.wide.ad.jp>
2015-07-19 05:04:19 +0900

    add cisco, alaxala, juniper and foundry backup command.

--- switch_backup.sh
+++ switch_backup.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-DATE=`date +%Y%m%d`
-DATE_YEAR=`date +%Y`
-DATE_MONTH=`date +%m`
-DATE_DAY=`date +%d`
+EXEC_DATE=`date +%Y%m%d`
+DATE_PATH=`date +%Y/%m/%d`
 TFTP_ROOT=/tftpboot
-BACKUP_DIR=${TFTP_ROOT}/${DATE_YEAR}/${DATE_MONTH}/${DATE_DAY}
+BACKUP_DIR=${TFTP_ROOT}/${DATE_PATH}
 HOSTS_FILE=
 ENCRYPTED_PASSWORD_FILE=
 ENCRYPTION_KEY_FILE=
-TFTPSERVER=10.0.0.1
+BACKUP_SERVER=10.0.0.1
 
 centre_backup() {
     USER="manager"
     TMP_FILE="autobackup.cfg"
-    BACKUP_FILE_PATH=${BACKUP_DIR}/${HOST_NAME}.${DATE}.cfg
-    CREATE_TMP_FILE="CREATE CONFIG=${TMP_FILE}"
-    BACKUP_COMMAND="UPLOAD METHO=tftp FILE=${TMP_FILE} DESTFILE=${BACKUP_FILE_PATH} SERVER=${TFTPSERVER}"
-    DELETE_TMP_FILE="DELTE FILE=${TMP_FILE}"
+    BACKUP_FILE_PATH=${BACKUP_DIR}/${HOST_NAME}.${EXEC_DATE}.cfg
+    CREATE_TMP_FILE_COMMAND="CREATE CONFIG=${TMP_FILE}"
+    BACKUP_COMMAND="UPLOAD METHO=tftp FILE=${TMP_FILE} DESTFILE=${BACKUP_FILE_PATH} SERVER=${BACKUP_SERVER}"
+    DELETE_TMP_FILE_COMMAND="DELTE FILE=${TMP_FILE}"
     touch ${BACKUP_FILE_PATH}
     chmod 666 ${BACKUP_FILE_PATH}
     
@@ -26,45 +24,89 @@ centre_backup() {
         spawn telnet $HOST_NAME
         expect \"login:\"   ; send \"${USER}\n\"
         expect \"Password:\"; send \"${PASSWORD}\n\"
-        expect \"Manager\"  ; send \"${CREATE_TMP_FILE}\n\"
+        expect \"Manager\"  ; send \"${CREATE_TMP_FILE_COMMAND}\n\"
         expect \"Manager\"  ; send \"${BACKUP_COMMAND}\n\"
-        expect \"Manager\"  ; send \"${DELETE_TMP_FILE}\n\"
+        expect \"Manager\"  ; send \"${DELETE_TMP_FILE_COMMAND}\n\"
     "
 }
 
 cisco_backup() {
     USER="elab"
+    BACKUP_FILE=${DATE_PATH}/${HOST_NAME}.${EXEC_DATE}.cfg
+    BACKUP_COMMAND="copy running-config tftp:${BACKUP_SERVER}/${BACKUP_FILE}"
+    touch ${TFTP_ROOT}/${BACKUP_FILE_PATH}
+    chmod 666 ${BACKUP_FILE_PATH}
+    
+    expect -c "
+        set timeout 20
+        spawn telnet $HOST_NAME
+        expect \"[Uu]sername:\" ; send \"${USER}\n\"
+        expect \"Password:\"    ; send \"${PASSWORD}\n\"
+        expect \"cisco\"        ; send \"en\n\"
+        expect \"Password:\"    ; send \"${PASSWORD}\n\"
+        expect \"cisco\"        ; send \"${BACKUP_COMMAND}\n\"
+        expect \"Address\"      ; send \"\n\"
+        expect \"Destination\"  ; send \"\n\"
+    "
+}
+
+alaxala_backup() {
+    USER="elab"
+    BACKUP_FILE=${DATE_PATH}/${HOST_NAME}.${EXEC_DATE}.cfg
+    BACKUP_COMMAND="copy running-config tftp:${BACKUP_SERVER}/${BACKUP_FILE}"
+    touch ${TFTP_ROOT}/${BACKUP_FILE_PATH}
+    chmod 666 ${BACKUP_FILE_PATH}
+    
+    expect -c "
+        set timeout 20
+        spawn telnet $HOST_NAME
+        expect \"[Ll]ogin:\" ; send \"${USER}\n\"
+        expect \"Password:\"    ; send \"${PASSWORD}\n\"
+        expect \"nec\"        ; send \"en\n\"
+        expect \"Password:\"    ; send \"${PASSWORD}\n\"
+        expect \"nec\"        ; send \"${BACKUP_COMMAND}\n\"
+        expect \"Configuration\"  ; send \"y\n\"
+    "
+}
+
+juniper_backup() {
+    USER="elab"
     TMP_FILE="autobackup.cfg"
-    BACKUP_FILE_PATH=${BACKUP_DIR}/${HOST_NAME}.${DATE}.cfg
-    CREATE_TMP_FILE="CREATE CONFIG=${TMP_FILE}"
-    BACKUP_COMMAND="UPLOAD METHO=tftp FILE=${TMP_FILE} DESTFILE=${BACKUP_FILE_PATH} SERVER=${TFTPSERVER}"
-    DELETE_TMP_FILE="DELTE FILE=${TMP_FILE}"
+    BACKUP_FILE_PATH=${BACKUP_DIR}/${HOST_NAME}.${EXEC_DATE}.cfg
+    CREATE_TMP_FILE_COMMAND="save ${TMP_FILE}"
+    BACKUP_COMMAND="file copy ${TMP_FILE} scp://${USER}@${BACKUP_SERVER}${BACKUP_FILE_PATH}"
+    DELETE_TMP_FILE_COMMAND="file delete ${TMP_FILE}"
     touch ${BACKUP_FILE_PATH}
     chmod 666 ${BACKUP_FILE_PATH}
     
     expect -c "
         set timeout 20
         spawn telnet $HOST_NAME
-        expect \"[Uu]sername:\"   ; send \"${USER}\n\"
-        expect \"Password:\"; send \"${PASSWORD}\n\"
-        expect \"cisco\"  ; send \"en\n\"
-        expect \"Password:\"; send \"${PASSWORD}\n\"
-        expect \"cisco\"  ; send \"${CREATE_TMP_FILE}\n\"
-        expect \"cisco\"  ; send \"${BACKUP_COMMAND}\n\"
-        expect \"cisco\"  ; send \"${DELETE_TMP_FILE}\n\"
+        expect \"[Ll]ogin:\"    ; send \"${USER}\n\"
+        expect \"Password:\"    ; send \"${PASSWORD}\n\"
+        expect \"elab@\"        ; send \"configure\n\"
+        expect \"elab@\"        ; send \"${CREATE_TMP_FILE_COMMAND}\n\"
+        expect \"elab@\"        ; send \"exit\n\"
+        expect \"elab@\"        ; send \"${BACKUP_COMMAND}\n\"
+        expect \"elab@\"        ; send \"${DELETE_TMP_FILE_COMMAND}\n\"
     "
 }
 
-alaxala_backup() {
-    echo alaxala
-}
-
-juniper_backup() {
-    echo juniper
-}
-
 foundry_backup() {
-    echo foundry
+    USER="elab"
+    BACKUP_FILE=${DATE_PATH}/${HOST_NAME}.${EXEC_DATE}.cfg
+    BACKUP_COMMAND="copy running-config tftp ${BACKUP_SERVER} ${BACKUP_FILE}"
+    touch ${TFTP_ROOT}/${BACKUP_FILE}
+    chmod 666 ${TFTP_ROOT}/${BACKUP_FILE}
+    
+    expect -c "
+        set timeout 20
+        spawn telnet $HOST_NAME
+        expect \"Please Enter Login Name:\" ; send \"${USER}\r\n\"
+        expect \"Please Enter Password:\" ; send \"${PASSWORD}\r\n\"
+        expect \"*foundry\"        ; send \"${BACKUP_COMMAND}\r\n\"
+        expect \"*Upload\"
+    "
 }
 
 backup() {
@@ -84,7 +126,7 @@ get_password() {
 	    exit 1
     fi
 
-    TMP_PASSWORD_FILE=tmp_password_${DATE}
+    TMP_PASSWORD_FILE=tmp_password_${EXEC_DATE}
     openssl enc -d -aes256 -in ${ENCRYPTED_PASSWORD_FILE} -out ${TMP_PASSWORD_FILE} -kfile ${ENCRYPTION_KEY_FILE}
     PASSWORD=`cat $TMP_PASSWORD_FILE`
     rm $TMP_PASSWORD_FILE    
